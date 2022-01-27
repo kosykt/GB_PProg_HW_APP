3d693e16b9b7544486f0ff4d310fe6acd52f6293
konstantin <kosvasl09@gmail.com>
2022-01-27 22:43:22 +0900

    переопределен onBackPressed

--- app/src/main/java/com/example/gb_pprog/presentation/MainActivity.kt
+++ app/src/main/java/com/example/gb_pprog/presentation/MainActivity.kt
@@ -1,14 +1,19 @@
 package com.example.gb_pprog.presentation
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.Navigation
 import com.example.gb_pprog.R
 import com.example.gb_pprog.databinding.ActivityMainBinding
 
+private const val FIRST_F = "first"
+private const val TIMER_F = "timer"
+
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private var fragmentOnView = FIRST_F
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,11 +23,13 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initFirstFragment() {
+        fragmentOnView = FIRST_F
         Navigation.findNavController(this, R.id.main_container)
             .popBackStack()
     }
 
     private fun initTimerFragment() {
+        fragmentOnView = TIMER_F
         Navigation.findNavController(this, R.id.main_container)
             .navigate(R.id.action_firstFragment_to_timerFragment)
     }
@@ -50,6 +57,14 @@ class MainActivity : AppCompatActivity() {
                 R.id.bottom_view_timer_fragment -> {}
                 else -> {}
             }
+        }
+    }
+
+    override fun onBackPressed() {
+        if (fragmentOnView == FIRST_F){
+            super.onBackPressed()
+        }else if (fragmentOnView == TIMER_F){
+            binding.mainBnv.selectedItemId = R.id.bottom_view_ff
         }
     }
 }
