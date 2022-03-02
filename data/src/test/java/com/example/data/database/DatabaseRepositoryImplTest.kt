f963a32fc12a2725e16359954b382d6a9b7183c9
konstantin <kosvasl09@gmail.com>
2022-03-03 01:21:38 +0900

    Баг, вызывает исключение NullPointerException

---
+++ data/src/test/java/com/example/data/database/DatabaseRepositoryImplTest.kt
@@ -0,0 +1,39 @@
+package com.example.data.database
+
+import com.example.gb_pprog.data.database.AppDatabase
+import com.example.gb_pprog.data.database.DatabaseRepositoryImpl
+import com.example.gb_pprog.data.database.model.RoomModel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.emitAll
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.runBlocking
+import org.junit.After
+import org.junit.Test
+import org.mockito.Mockito
+import org.mockito.kotlin.mock
+
+class DatabaseRepositoryImplTest {
+
+    private val db = mock<AppDatabase>()
+
+    @After
+    fun tearDown(){
+        Mockito.reset(db)
+    }
+
+    @Test
+    fun should_return_notNull() {
+        val databaseRepositoryImpl = DatabaseRepositoryImpl(db)
+        val testData = listOf<RoomModel>(
+            RoomModel("test0", "data0"),
+            RoomModel("test1", "data1"),
+            RoomModel("test2", "data2"),
+        )
+        runBlocking {
+            Mockito.`when`(db.roomDao().getAll()).thenReturn(flow {
+                /** вызывает исключение NullPointerException **/
+                emit(testData)
+            })
+        }
+    }
+}
