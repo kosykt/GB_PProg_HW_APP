0c2fc4b5e073f1e0b98aeb5df6b5ce4a06ab2286
konstantine <kosykt>
2022-03-15 03:41:00 +0900

    добавлен HttpLoggingInterceptor

--- app/src/main/java/com/example/gb_pprog/di/modules/singletons/RetrofitModule.kt
+++ app/src/main/java/com/example/gb_pprog/di/modules/singletons/RetrofitModule.kt
@@ -3,6 +3,8 @@ package com.example.gb_pprog.di.modules.singletons
 import com.example.gb_pprog.data.network.RetrofitService
 import dagger.Module
 import dagger.Provides
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
@@ -14,8 +16,9 @@ class RetrofitModule {
 
     @Singleton
     @Provides
-    fun provideRetrofit(): Retrofit {
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
         return Retrofit.Builder()
+            .client(okHttpClient)
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
@@ -25,5 +28,17 @@ class RetrofitModule {
     @Singleton
     fun provideApi(retrofit: Retrofit): RetrofitService {
         return retrofit.create(RetrofitService::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(interceptor: HttpLoggingInterceptor): OkHttpClient {
+        return OkHttpClient.Builder().addInterceptor(interceptor).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideOkHttpInterceptor(): HttpLoggingInterceptor {
+        return HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BASIC)
     }
 }
