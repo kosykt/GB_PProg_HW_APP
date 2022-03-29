e0b5b716493ceb8ac8efb5909af7a5b288dc24f1
konstantine <kosykt>
2022-03-29 21:36:16 +0900

    тест DetailsFragmentTest.kt

--- app/src/androidTest/java/com/example/gb_pprog/ui/detailsfragment/DetailsFragmentTest.kt
+++ app/src/androidTest/java/com/example/gb_pprog/ui/detailsfragment/DetailsFragmentTest.kt
@@ -3,6 +3,10 @@ package com.example.gb_pprog.ui.detailsfragment
 import android.os.Bundle
 import androidx.fragment.app.testing.FragmentScenario
 import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.navArgs
+import androidx.navigation.testing.TestNavHostController
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
@@ -10,6 +14,7 @@ import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.example.gb_pprog.R
 import com.example.mytranslator.ui.detailsfragment.DetailsFragment
+import com.example.mytranslator.ui.detailsfragment.DetailsFragmentArgs
 import com.example.mytranslator.ui.detailsfragment.DetailsModel
 import org.junit.After
 import org.junit.Before
@@ -20,6 +25,9 @@ import org.junit.runner.RunWith
 class DetailsFragmentTest {
 
     private lateinit var scenario: FragmentScenario<DetailsFragment>
+    private val navController = TestNavHostController(
+        ApplicationProvider.getApplicationContext()
+    )
     private val model = DetailsModel(
         word = "word",
         transcription = "transcription",
@@ -33,11 +41,14 @@ class DetailsFragmentTest {
 
     @Before
     fun setup() {
-        TODO("необходимо передать не fragmentArgs, а navArgs")
+//        TODO("необходимо передать не fragmentArgs, а navArgs")
         scenario = launchFragmentInContainer(
-            themeResId = R.style.Theme_GB_PProg,
-            fragmentArgs = bundle
+            themeResId = R.style.Theme_GB_PProg
         )
+        scenario.onFragment { fragment ->
+            navController.setGraph(R.navigation.main_navigation)
+            Navigation.setViewNavController(fragment.requireView(), navController)
+        }
     }
 
     @Test
