e6a5f68aa835d889348f2f0151d9ae7fd71aac4a
konstantine <kosykt>
2022-03-29 15:00:47 +0900

    выпилил robolectric из-за конфликта с fragment-testing

--- buildSrc/src/main/java/Dependecies.kt
+++ buildSrc/src/main/java/Dependecies.kt
@@ -131,6 +131,8 @@ object TestImpl {
 
     const val ext_junit = "androidx.test.ext:junit:${Versions.extjUnit}"
     const val espresso_core = "androidx.test.espresso:espresso-core:${Versions.espressoCore}"
+    const val fragment_testing = "androidx.fragment:fragment-testing:1.4.1"
+    const val espresso_contrib = "androidx.test.espresso:espresso-contrib:3.4.0"
 
     const val mockito_core = "org.mockito:mockito-core:${Versions.mockito}"
     const val mockito_kotlin = "org.mockito.kotlin:mockito-kotlin:${Versions.mockito}"
@@ -139,6 +141,7 @@ object TestImpl {
     const val arch_core = "androidx.arch.core:core-testing:${Versions.archCore}"
 
     const val ui_automator = "androidx.test.uiautomator:uiautomator:2.2.0"
+    const val test_rules = "androidx.test:rules:1.4.0"
 
     const val robolectric = "org.robolectric:robolectric:4.5.1"
     const val test_core = "androidx.test:core:1.4.0"
